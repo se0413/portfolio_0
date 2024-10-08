1f05bbad6cbce936d64caf2d57b66452167a831d
cat0413 <tldms7133@naver.com>
2024-10-08 15:16:41 +0900

    1008

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -3,11 +3,11 @@ $(document).ready(function(){
 
 		navigation: true, 
 		navigationPosition: 'left', 
-		navigationTooltips: ['MAIN', 'I AM', 'PROJECT1', 'PROJECT12','PROJECT13','PROJECT4','FINISH'], 
+		navigationTooltips: ['MAIN', 'I AM', 'PROJECT1', 'PROJECT12','PROJECT13','PROJECT4','EMOTICON','DRAW','FINISH'], 
 		showActiveTooltip: true, 
 		
 		lockAnchors: true,
-		anchors: ['link1', 'link2', 'link3', 'link4', 'link5', 'link6'], 
+		anchors: ['link1', 'link2', 'link3', 'link4', 'link5', 'link6', 'link7', 'link8'], 
 
 		autoScrolling:true, 
 		scrollHorizontally: true,
