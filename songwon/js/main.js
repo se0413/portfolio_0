b32ef30f45a17f624a8d093aeff1b2f9270b7430
cat0413 <tldms7133@naver.com>
2024-10-17 13:50:36 +0900

    1017

--- songwon/js/main.js
+++ songwon/js/main.js
@@ -27,4 +27,25 @@ $(document).ready(function(){
    $('.visual .btn_wrap button.btn_play').on('click', function(){
         visual_swiper.autoplay.start(); 
     })
+
+	let tab_btn = $('.notice .tap_area .tap_btn ul li') /*변수 이름을 정해줌*/
+    let tab_name
+    let tab_cnt = $('.notice .tap_area .tap_cnt div[role="tabpanel"]')
+    let tab_cnt_prant = $('.notice .tap_area .tap_cnt')
+
+    tab_btn.on('click', function(){
+        tab_btn.removeClass('active')
+        $(this).addClass('active')
+
+        tab_btn.attr('aria-selected', 'false')
+        $(this).attr('aria-selected', 'true')
+
+        tab_name = $(this).attr('aria-controls')
+        tab_name = '#'+ tab_name /*id 클래스를 추가로 삽입*/
+        console.log(tab_name)
+
+        tab_cnt.removeClass('active')
+        tab_cnt_prant.find(tab_name).addClass('active')
+        
+     })
 })
