1103ec49428175c0ccb1ac4df53eb9c5700a3918
cat0413 <tldms7133@naver.com>
2024-10-18 17:57:58 +0900

    1018

--- songwon/js/main.js
+++ songwon/js/main.js
@@ -28,7 +28,7 @@ $(document).ready(function(){
         visual_swiper.autoplay.start(); 
     })
 
-	let tab_btn = $('.notice .tap_area .tap_btn ul li') /*변수 이름을 정해줌*/
+	let tab_btn = $('.notice .tap_area .tap_btn ul li') 
     let tab_name
     let tab_cnt = $('.notice .tap_area .tap_cnt div[role="tabpanel"]')
     let tab_cnt_prant = $('.notice .tap_area .tap_cnt')
@@ -41,11 +41,33 @@ $(document).ready(function(){
         $(this).attr('aria-selected', 'true')
 
         tab_name = $(this).attr('aria-controls')
-        tab_name = '#'+ tab_name /*id 클래스를 추가로 삽입*/
+        tab_name = '#'+ tab_name
         console.log(tab_name)
 
         tab_cnt.removeClass('active')
         tab_cnt_prant.find(tab_name).addClass('active')
         
      })
+     const news_swiper = new Swiper('.news .swiper', { 
+        slidesPerView: 2, 
+        spaceBetween: 16, 
+        breakpoints: {
+            768: { 
+                slidesPerView: 4,
+                spaceBetween: 30,
+            },
+            1024: {
+                slidesPerView: 4,
+                spaceBetween: 24,
+            },
+        },
+        centeredSlides: false,
+        loop: true, 
+
+        scrollbar: {
+            el: ".news .swiper-scrollbar",
+            hide: false,
+            draggable: true,
+        },
+    });
 })
