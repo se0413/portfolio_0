22265ca6965d6bc6c67ea46e93851040f4d5aa61
cat0413 <tldms7133@naver.com>
2024-11-10 17:15:00 +0900

    1110.2

--- songwon2/js/main.js
+++ songwon2/js/main.js
@@ -19,79 +19,30 @@ $(document).ready(function(){
 		},
 	
 	});
-
 	$('.visual .btn_wrap button.btn_stop').on('click', function(){
         visual_swiper.autoplay.stop();  
 	})
-
    $('.visual .btn_wrap button.btn_play').on('click', function(){
         visual_swiper.autoplay.start(); 
     })
 
-	let tab_btn = $('.notice .tap_area .tap_btn ul li') 
-    let tab_name
-    let tab_cnt = $('.notice .tap_area .tap_cnt div[role="tabpanel"]')
-    let tab_cnt_prant = $('.notice .tap_area .tap_cnt')
-
-    tab_btn.on('click', function(){
-        tab_btn.removeClass('active')
-        $(this).addClass('active')
-
-        tab_btn.attr('aria-selected', 'false')
-        $(this).attr('aria-selected', 'true')
-
-        tab_name = $(this).attr('aria-controls')
-        tab_name = '#'+ tab_name
-        console.log(tab_name)
-
-        tab_cnt.removeClass('active')
-        tab_cnt_prant.find(tab_name).addClass('active')
-        
-     })
-
-     const news_swiper = new Swiper('.news .swiper', { 
-        slidesPerView: 1, 
-        spaceBetween: 14, 
+	const bookmark_swiper = new Swiper('.bookmark .swiper', { 
+        spaceBetween: 16,
         breakpoints: {
-            560: {
-                slidesPerView: 2,
-                spaceBetween: 24,
+            1400: {  
+                slidesPerView: 7,
+			    spaceBetween: 10,
             },
-            1024: {
-                slidesPerView: 4,
-                spaceBetween: 24,
-            },
-        },
-        centeredSlides: false,
-        loop: true, 
-
-        scrollbar: {
-            el: ".news .swiper-scrollbar",
-            hide: false,
-            draggable: true,
-        },
-    });
-
-    const sns_swiper = new Swiper('.sns .swiper', { 
-        slidesPerView: 1, 
-        spaceBetween: 14, 
-        breakpoints: {
-            560: {
-                slidesPerView: 2,
-                spaceBetween: 24,
-            },
-            1024: {
-                slidesPerView: 4,
-                spaceBetween: 24,
+            1070: {  
+                slidesPerView: 5,
+			    spaceBetween: 5,
             },
         },
-        centeredSlides: true,
-        loop: true, 
-
-        scrollbar: {
-            el: ".news .swiper-scrollbar",
-            hide: false,
-            draggable: true,
+        centeredSlides: false, 
+        loop: false,  
+        navigation: {
+            nextEl: '.swiper .btn_next',
+            prevEl: '.swiper .btn_prev',
         },
     });
 })
