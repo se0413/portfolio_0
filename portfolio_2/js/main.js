59737c6a6fca61369850e7710fb8c4135777e4c0
cat0413 <tldms7133@naver.com>
2024-11-20 14:06:46 +0900

    1120

--- portfolio_2/js/main.js
+++ portfolio_2/js/main.js
@@ -1,37 +1,39 @@
 $(document).ready(function(){
-	const myFullpage = new fullpage('#fullpage', {  
-
-		navigation: true, 
-		navigationPosition: 'left', 
-		navigationTooltips: ['MAIN', 'I AM', 'PROJECT1', 'PROJECT12','PROJECT13','PROJECT4','CARD NEWS','EMOTICON','DRAW','FINISH'], 
-		showActiveTooltip: true, 
-		
-		lockAnchors: true,
-		anchors: ['link1', 'link2', 'link3', 'link4', 'link5', 'link6', 'link7', 'link8', 'link9'], 
-
-		autoScrolling:true, 
-		scrollHorizontally: true,
-
-		verticalCentered: true, 
-		
-		scrollOverflow: false, 
-
-		afterLoad: function(origin, destination, direction, trigger){
-			if(destination.index == 1){
-				$('#fp-nav').addClass('w')
-			}else{
-				$('#fp-nav').removeClass('w')
+	
+	let menuName = $('.header') 
+	let menuItem = $('.header ul li') 
+	let sectionName
+	let moveTop
+	let areaTop
+	let areaH
+	let areaName
+	let scrollTop
+	menuItem.on('click', function(){
+		sectionName = $(this).attr('data-link')
+		moveTop = $('*[data-menu="'+sectionName+'"]').offset().top - menuName.height()
+		$('html, body').animate({
+			scrollTop : moveTop
+		}, 500)
+	})
+	menuChk()
+	$(window).scroll(function(){
+		menuChk()
+	})
+	function menuChk(){
+		scrollTop = $(window).scrollTop()
+		$.each($('*[data-menu]'), function(idx, item){
+			areaTop = $('*[data-menu]').eq(idx).offset().top
+			areaH = $('*[data-menu]').eq(idx).height()
+			areaName = $('*[data-menu]').eq(idx).attr('data-menu')
+			if((scrollTop >= areaTop - menuName.height()) && (scrollTop < areaTop + areaH - menuName.height())){
+				menuItem.removeClass('active')
+				menuItem.siblings('[data-link="'+areaName+'"]').addClass('active')
+			}else if(scrollTop < $('*[data-menu]').first().offset().top){
+				menuItem.removeClass('active')
+			}else if(scrollTop > $('*[data-menu]').last().offset().top + $('*[data-menu]').last().height()){
+				menuItem.removeClass('active')
 			}
-		},
-
-		responsiveWidth: 640 
-	});
-
-	$(window).on('scroll mousemove', function(e){  /* html cursor가 마우스 포인터를 따라다니게 하는 값 */
-		$('.cursor').css('left', e.pageX + 'px');
-		$('.cursor').css('top', e.pageY + 'px');
-	});
-	$('a').hover(function(){ /* a에 마우스를 올렸을때만 on 클래스 주기 */
-		$('.cursor').toggleClass('on');
-	});
+		});
+	}
+	
 })
