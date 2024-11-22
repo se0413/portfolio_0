075d7a1b6ab71a1219b4e5d2f8eabcb18980c6b8
cat0413 <tldms7133@naver.com>
2024-11-22 16:12:04 +0900

    .

--- portfolio_2/js/main.js
+++ portfolio_2/js/main.js
@@ -37,5 +37,25 @@ $(document).ready(function(){
 		});
 	}
 
-	
+	let tab_btn = $('.another .tap_area .tap_btn ul li') 
+    let tab_name
+    let tab_cnt = $('.another .tap_area .tap_cnt div[role="tabpanel"]')
+    let tab_cnt_prant = $('.another .tap_area .tap_cnt')
+
+    tab_btn.on('click', function(){
+        tab_btn.removeClass('active')
+        $(this).addClass('active')
+
+        tab_btn.attr('aria-selected', 'false')
+        $(this).attr('aria-selected', 'true')
+
+        tab_name = $(this).attr('aria-controls')
+        tab_name = '#'+ tab_name
+        console.log(tab_name)
+
+        tab_cnt.removeClass('active')
+        tab_cnt_prant.find(tab_name).addClass('active')
+        
+    })
+
 })
